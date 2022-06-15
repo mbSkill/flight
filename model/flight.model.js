58953565f05768a43b73d01e40493f06a8b19c19
Matt <matthewbosticdev@gmail.com>
2022-06-15 09:11:03 -0600

    added nodemon + changed package structure

---
+++ model/flight.model.js
@@ -0,0 +1,13 @@
+/**
+ * Flight Number
+ * Departure Date
+ * Arrival date
+ * departure time
+ * arrival time
+ * departure airport
+ * arrival airport
+ * current number of passengers
+ * passenger limit
+ */
+
+
